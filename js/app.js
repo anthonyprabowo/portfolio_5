546071aa0ed62cf3c553121e0e5b8a10f649165b
Anthony Prabowo <anthonyprabowo@users.noreply.github.com>
2020-11-08 19:32:35 -0800

    adding comment to the javascript

--- js/app.js
+++ js/app.js
@@ -11,9 +11,14 @@ window.addEventListener('load', function() {
 // creating sort filter (my version)
 
 function searchResults() {
+    // collecting all images from picture from the picture class
     const images = document.querySelectorAll('.picture');
+    
+    // getting the input from the text box and convert it to lowercase
     let input = document.getElementById('search');
     let filter = input.value.toLowerCase();
+
+    // Loop through all the images and hide the picture that has no match.
     for( let i = 0; i < images.length; i++) {
         let dataCaption = images[i].getAttribute('data-caption');
         if(dataCaption.includes(filter)) {
